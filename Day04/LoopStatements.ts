330bd863cf119ce86c520f6c1d3b227243e234ee
ChathurangaPri <hachathurangapriyadarshani@gmail.com>
2026-07-15 11:17:49 +1200

    Conditional and looping statements

---
+++ Day04/LoopStatements.ts
@@ -0,0 +1,51 @@
+// While loop - executes until the condition is met
+
+let i:number = 1;
+/* while (i<=10){
+    console.log(i);
+    i++;
+} */
+
+/* while(i%2 == 0 && i<=10){
+    console.log(i);
+    i+=2;
+} */
+
+/* while(i>=1){
+    console.log(i);
+    i--;
+} */
+
+// do while - executes at least once before condition check
+
+/* do{
+    console.log(i);  
+    i++; 
+}while(i<1) */
+
+// For loop
+/* for(let j:number = 1; j<=10; j++){
+    console.log(j);    
+} */
+
+/* for(let j:number = 2; j<=10; j+=2 ){
+    console.log(j);   
+} */
+
+// break statemet
+/* for(let k:number =1; k<=10; k++){
+    if(k==5){
+        break;
+    }
+    console.log(k);
+    
+} */
+
+// continue statement
+for(let k:number =1; k<=10; k++){
+    if(k==5){
+        continue;
+    }
+    console.log(k);
+    
+}
